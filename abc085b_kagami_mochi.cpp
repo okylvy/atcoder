79c55df02a0d20c8e83e8e4865f6ea0b5359a8b0
okylvy <okylvy@yahoo.co.jp>
2021-03-14 16:54:06 +0900

    Add ABC085B - Kagami Mochi

--- abc085b_kagami_mochi.cpp
+++ abc085b_kagami_mochi.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i=0; i != n; i++) {
+        cin >> vec.at(i);
+    }
+    int cnt = 0, prev = 0;
+    for (int i=0; i != n; i++) {
+        decltype(vec)::iterator max_val = max_element(vec.begin(), vec.end());
+        if (prev == 0 || prev > *max_val) {
+            cnt++;
+        }
+        prev = *max_val;
+        vec.erase(max_val);
+    }
+    cout << cnt;
 }
