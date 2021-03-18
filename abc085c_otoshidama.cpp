f4a954500c09208d91c4f7abab973c74868285ab
okylvy <okylvy@yahoo.co.jp>
2021-03-18 21:22:38 +0900

    Add ABC085C - Otoshidama

---
+++ abc085c_otoshidama.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, y;
+    cin >> n >> y;
+
+    for (int i=0; i<=n; i++) {
+        for (int j=0; j<=(n-i); j++) {
+            if ((i*9000 + j*4000 + n*1000) == y) {
+                std::cout << i << " " << j << " " << n-i-j;
+                return 0;
+            }
+        }
+    }
+    std::cout << -1 << " " << -1 << " " << -1;
+}
