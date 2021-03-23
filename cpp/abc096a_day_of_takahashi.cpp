6b7bfab0f72670d3ac4c45a5ad3ee6cd820fc7cf
okylvy <okylvy@yahoo.co.jp>
2021-03-23 18:44:18 +0900

    Modify the answer of Day of Takahashi.

--- cpp/abc096a_day_of_takahashi.cpp
+++ cpp/abc096a_day_of_takahashi.cpp
@@ -5,11 +5,8 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    int cnt=0;
-    for (int i=1; i != a+1; i++) {
-        if (b >= i) {
-            cnt++;
-        }
-    }
-    cout << cnt;   
+    if (a <= b)
+        cout << a;
+    else
+        cout << a-1;
 }
